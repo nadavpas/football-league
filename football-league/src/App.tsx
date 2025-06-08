2bd58a91ceb194b9a4573edd02ffa69462d95760
nadavpas <nadavp@moveo.com>
2025-06-08 15:24:39 +0300

    return a dict of the teams when clicking on the league

--- football-league/src/App.tsx
+++ football-league/src/App.tsx
@@ -1,11 +1,12 @@
 import {fetchTeams} from './utils/teams'
 import './App.css'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
+  type LeagueType = "Premier League" | "La Liga" | "Serie A" | "Bundesliga" | "Ligue 1"
   const [teams,setTeams] = useState([]);
-  const [league,setLeague] = useState("Premier League");
-  const leaguesList = {
+  const [league,setLeague] = useState<LeagueType>("Premier League");
+  const leaguesList: Record<LeagueType,number> = {
     "Premier League": 4328,
     "La Liga": 4335,
     "Serie A": 4332,
@@ -13,11 +14,31 @@ function App() {
     "Ligue 1": 4334
   };
 
+  useEffect(()=>{
+    const loadTeams = async () => {
+      try{
+        const league_id:number = leaguesList[league];
+        const teamsData = await fetchTeams(league_id);
+        const teams_dict = teamsData.map((team: any) => ({
+        name: team.strTeam,
+        logo: team.strLogo
+    }));
+        setTeams(teams_dict);
+      }catch(e){
+        console.log(e)
+      }
+    };
+    loadTeams();
+  },[league])
+
 
   return (
     <ul>
-      {Object.keys(leaguesList).map(league => (
-        <li>{league}</li>
+      {Object.keys(leaguesList).map(l => (
+        <li key={l} onClick={()=> {
+          setLeague(l as LeagueType);
+          console.log(teams)
+        }}>{l}</li>
       ))}
     </ul>
   )
--- football-league/src/utils/teams.ts
+++ football-league/src/utils/teams.ts
@@ -1,6 +1,6 @@
-const Base_URL  ='hhttps://www.thesportsdb.com/api/v1/json/123/search_all_teams.php';
+const Base_URL  ='https://www.thesportsdb.com/api/v1/json/123/search_all_teams.php';
 
-export async function fetchTeams(league_id:string) {
+export async function fetchTeams(league_id:number) {
     const response = await fetch(`${Base_URL}?id=${league_id}`);
 
     if (!response.ok){
@@ -8,5 +8,6 @@ export async function fetchTeams(league_id:string) {
     }
 
     const data = await response.json();
+    
     return data.teams;
 }
