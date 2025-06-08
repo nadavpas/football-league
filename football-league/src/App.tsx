432c8dc4478fd4538a9292c70e076f876dd9cd3b
nadavpas <nadavp@moveo.com>
2025-06-08 13:41:06 +0300

    listing 5 leagues

--- football-league/src/App.tsx
+++ football-league/src/App.tsx
@@ -1,8 +1,11 @@
 import {fetchTeams} from './utils/teams'
 import './App.css'
+import { useState } from 'react';
 
 function App() {
-  const leagues = {
+  const [teams,setTeams] = useState([]);
+  const [league,setLeague] = useState("Premier League");
+  const leaguesList = {
     "Premier League": 4328,
     "La Liga": 4335,
     "Serie A": 4332,
@@ -10,10 +13,13 @@ function App() {
     "Ligue 1": 4334
   };
 
-  
 
   return (
-    <h1>HELLO</h1>
+    <ul>
+      {Object.keys(leaguesList).map(league => (
+        <li>{league}</li>
+      ))}
+    </ul>
   )
 }
 
