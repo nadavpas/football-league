81c998c9016d9c52e281aa8e99f043ebe55ef60e
nadavpas <nadavp@moveo.com>
2025-06-09 15:42:09 +0300

    improved UI

--- football-league/src/App.tsx
+++ football-league/src/App.tsx
@@ -1,59 +1,63 @@
-import {fetchTeams} from './utils/teams'
-import LeagueNavbar from './components/LeagueNavbar'
-import TeamCard from './components/TeamCard'
-import './App.css'
-import { useEffect, useState } from 'react';
-import { Container,Grid } from '@mui/material';
+import { fetchTeams } from "./utils/teams";
+import LeagueNavbar from "./components/LeagueNavbar";
+import TeamCard from "./components/TeamCard";
+import "./App.css";
+import { useEffect, useState } from "react";
+import { Grid } from "@mui/material";
 
 function App() {
-  type Team = {name:string, logo:string};
-  type LeagueType = "Premier League" | "La Liga" | "Serie A" | "Bundesliga" | "Ligue 1" 
-  const [teams,setTeams] = useState<Team[]>([]);
-  const [league,setLeague] = useState<LeagueType>("Bundesliga");
-  const leaguesList: Record<LeagueType,number> = {
+  // create types
+  type Team = { name: string; logo: string };
+  type LeagueType =
+    | "Premier League"
+    | "La Liga"
+    | "Serie A"
+    | "Bundesliga"
+    | "Ligue 1";
+  // states declerations
+  const [teams, setTeams] = useState<Team[]>([]);
+  const [league, setLeague] = useState<LeagueType>("Bundesliga");
+  // init a dict containing name -> id
+  const leaguesList: Record<LeagueType, number> = {
     "Premier League": 4328,
     "La Liga": 4335,
     "Serie A": 4332,
-    "Bundesliga": 4331,
-    "Ligue 1": 4334
+    Bundesliga: 4331,
+    "Ligue 1": 4334,
   };
-
-  useEffect(()=>{
+  // useEffect thet will trigger the get request on each league selection
+  useEffect(() => {
     const loadTeams = async () => {
-      try{
-        const league_id:number = leaguesList[league];
+      try {
+        const league_id: number = leaguesList[league];
         const teamsData = await fetchTeams(league_id);
         const teams_dict = teamsData.map((team: any) => ({
-        name: team.strTeam,
-        logo: team.strLogo
-    }));
+          name: team.strTeam,
+          logo: team.strLogo,
+        }));
         setTeams(teams_dict);
-      }catch(e){
-        console.log(e)
+      } catch (e) {
+        console.log(e);
       }
     };
     loadTeams();
-    },[league])
-
+  }, [league]);
 
   return (
     <>
-     <LeagueNavbar 
-    leagues={Object.keys(leaguesList) as LeagueType[]}
-    onLeagueSelect={setLeague}/>
-    <Container>
-      <Grid container spacing={2} sx={{mt: 2}}>
-        {teams.map(t => (
-        <Grid>
-          <TeamCard team={t as Team}/> 
+      <LeagueNavbar
+        leagues={Object.keys(leaguesList) as LeagueType[]}
+        onLeagueSelect={setLeague}
+      />
+      <Grid container spacing={2} sx={{ paddingTop: 3 }}>
+        <Grid sx={{ padding: 2 }}>
+          {teams.map((t) => (
+            <TeamCard team={t as Team} />
+          ))}
         </Grid>
-      ))}
       </Grid>
-    </Container>
-      
     </>
-   
   );
-};
+}
 
-export default App
+export default App;
--- football-league/src/components/TeamCard.tsx
+++ football-league/src/components/TeamCard.tsx
@@ -1,22 +1,14 @@
-
-import { Card,CardContent, Typography, Avatar, Box } from "@mui/material";
-type Team = {name:string, logo:string};
+import { Card } from "@mui/material";
+type Team = { name: string; logo: string };
 interface TeamCardProp {
-    team: Team
-};
-function TeamCard({team} : TeamCardProp){
-    return (
-        <Card>
-            <CardContent>
-                <Box sx={{display: 'flex', alignItems:'center'}}>
-                    <Avatar src={team.logo} alt={team.name}>
-                        <Typography variant="h6" component="h2">
-                            {team.name}
-                        </Typography>
-                     </Avatar>
-                </Box>
-            </CardContent>
-        </Card>
-    );
-};
-export default TeamCard
+  team: Team;
+}
+function TeamCard({ team }: TeamCardProp) {
+  return (
+    <Card sx={{ border: "1px solid black" }}>
+      <p>{team.name}</p>
+      <img src={team.logo} alt={team.name} />
+    </Card>
+  );
+}
+export default TeamCard;
