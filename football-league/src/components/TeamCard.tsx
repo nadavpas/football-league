40a6f8f482126ee373588833af126f8233728d45
nadavpas <nadavp@moveo.com>
2025-06-10 11:04:35 +0300

    changed the background color of the card to imrove visibillity

--- football-league/src/components/TeamCard.tsx
+++ football-league/src/components/TeamCard.tsx
@@ -5,7 +5,7 @@ interface TeamCardProp {
 }
 function TeamCard({ team }: TeamCardProp) {
   return (
-    <Card sx={{ border: "1px solid black" }}>
+    <Card sx={{ border: "1px solid black" , backgroundColor:"#f3efef"}}>
       <p>{team.name}</p>
       <img src={team.logo} alt={team.name} />
     </Card>
